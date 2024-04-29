04fc5f7293a6c883371e0171a49d22224a38933c
kiriku22 <114250566+kiriku22@users.noreply.github.com>
2024-04-29 03:07:06 +0000

    update

--- FakeStoreApi/FakeStoreApi/obj/Debug/net8.0/FakeStoreApi.AssemblyInfo.cs
+++ FakeStoreApi/FakeStoreApi/obj/Debug/net8.0/FakeStoreApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FakeStoreApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c29947a0143e77abf28a2ccd109fa3cbc51fc6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ac43da678a24c6e5a0610409caf5849be5b903f")]
 [assembly: System.Reflection.AssemblyProductAttribute("FakeStoreApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FakeStoreApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
